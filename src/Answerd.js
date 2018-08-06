ee60941243d30a9db288ccb1bc3e5a72a848a17c
ork23 <p.anderson2390@gmail.com>
2018-08-05 23:40:58 -0400

    style: add style code to answerd

--- src/Answerd.js
+++ src/Answerd.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import next from './next.svg'
+
 class Answerd extends Component{
     render(){
         return(
             <div class='answerd'>
-            <img src={next} height="30px" width="30px"/><h3>respuesta 1</h3>
-            </div>
+           </div>
         );
     }
 }
