e2445a3675714755152bc02172ba3c790133c2ea
ork23 <p.anderson2390@gmail.com>
2018-08-08 22:07:53 -0400

    style: Add component Userlist and style

---
+++ src/UserList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './UserList.css';
+
+class ListUser extends Component{
+    render(){
+        return(
+            <div>
+                <div className='listUser'>
+                    <div>Usuario1</div>
+                    <div>Usuario2</div>
+                    <div>Usuario3</div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Repo
